d43f7e5c9f2670ceecf1b01b2499ed4b7e4d6d3f
TJ <tjb2597@rit.edu>
2015-07-24 17:02:29 -0400

    Functionality work

--- Is-This-Love/src/scripts/controls.js
+++ Is-This-Love/src/scripts/controls.js
@@ -1,26 +1,42 @@
 var audioPlayer = document.getElementById("audio");
-
-
 var playPause 	= document.getElementById("play-pause");
 var repeat 		= document.getElementById("repeat");
 var amount 		= document.getElementById("amount");
 var elapsedTime = document.getElementById("elapsed-time");
 var totalTime 	= document.getElementById("total-time");
-totalTime.innerHTML = calcFormattedLength();
 
-var playing = false;
+var playing 	= false;
+var repeating	= false;
 var i = 0;
 var elapsed = 0;
 var seconds = 0;
 var minutes = 0;
-// var length 	= calcLength();
+var length = 0;
+
+function calcFormattedLength(){
+	var formattedMins = Math.floor(length / 60);
+	var formattedSecs = length % 60;
+	if(formattedSecs < 10){
+		formattedSecs = "0" + formattedSecs;
+	}
+
+	return formattedMins + ":" + formattedSecs;
+}
 
 function checkTime(){
 
 	//Stop interval checking if percent at 100
 	if(percent >= 100){
-		clearInterval(i);
-		return;
+		if(repeating){
+			i = 0;
+			elapsed = 0;
+			seconds = 0;
+			minutes = 0;
+		}
+		else{
+			clearInterval(i);
+			return;
+		}
 	}
 
 	elapsed++ 
@@ -43,30 +59,31 @@ function checkTime(){
 	elapsedTime.innerHTML = time;
 }
 
-function calcFormattedLength(){
-	var length = audioPlayer.seekable.end();
-	var formattedTime = document.getElementById("total-time").innerHTML;
-	formattedMins = parseInt(formattedTime.substr(0, formattedTime.indexOf(':')));
-	formattedSecs = parseInt(formattedTime.substr(formattedTime.indexOf(':') + 1));
-
-	return (60 * formattedMins) + formattedSecs;
-}
 
 playPause.addEventListener("click", function(){
 	playPause.classList.toggle("paused");
 
 	if(!playing){
 		playing = true;
+		begun = true;
 		audioPlayer.play();
 		i = setInterval(checkTime, 1000);
 	} else{
 		playing = false;
+		audioPlayer.pause();
+		clearInterval(i);
 	}
 });
 
 repeat.addEventListener("click", function(){
 	repeat.classList.toggle("on-repeat");
+	repeating = true;
 });
+
+audioPlayer.onloadeddata = function(){
+	length = Math.ceil(audioPlayer.duration);
+	totalTime.innerHTML = calcFormattedLength();
+}
 
 
 
