b593b74511ae20f66cc8c98a026eded9c1e48611
TJ <tjb2597@rit.edu>
2015-07-29 00:33:07 -0400

    Visualizer

--- Is-This-Love/src/scripts/controls.js
+++ Is-This-Love/src/scripts/controls.js
@@ -28,7 +28,7 @@ function calcFormattedLength(){
 }
 
 function checkTime(){
-	//Stop interval checking if percent at 100
+	// If percent at 100
 	if(percent >= 100){
 		if(repeating){
 			i = 0;
@@ -45,15 +45,14 @@ function checkTime(){
 	elapsed++;
 	seconds = elapsed % 60;
 	minutes = Math.floor(elapsed / 60);
-	percent = elapsed / length * 100;
-	amount.style.width = percent + "%";
-
-	if(seconds < 10){
+	if(seconds < 10)
 		seconds = "0" + seconds;
-	}
 
 	time = minutes + ":" + seconds;
 	elapsedTime.innerHTML = time;
+
+	percent = elapsed / length * 100;
+	amount.style.width = percent + "%";
 }
 
 audioPlayer.onloadeddata = function(){
@@ -67,6 +66,7 @@ playPause.addEventListener("click", function(){
 
 	if(playing){
 		audioPlayer.play();
+		// renderFrame();
 		i = setInterval(checkTime, 1000);
 	} else{
 		audioPlayer.pause();
@@ -98,6 +98,149 @@ forward.addEventListener("click", function(){
 	audioPlayer.currentTime = elapsed;
 });
 
-particlesJS.load('particles-js', 'dist/particles.json', function() {
-  console.log('callback - particles.js config loaded');
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
 });
+
+var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
+window.addEventListener("load", initMp3Player, false);
+function initMp3Player(){
+	context = new AudioContext();
+	analyser = context.createAnalyser();
+	canvas = document.getElementById('analyser_render');
+	canvas.width=1000;//horizontal resolution (?) - increase for better looking text
+	canvas.height=200;//vertical resolution (?) - increase for better looking text
+	canvas.setAttribute("width", canvas.width);//actual width of canvas
+	canvas.setAttribute("height", canvas.height);//actual width of canvas
+	ctx = canvas.getContext('2d');
+
+	source = context.createMediaElementSource(audioPlayer);
+	source.connect(analyser);
+	analyser.connect(context.destination);
+
+	frameLooper();
+}
+
+function frameLooper(){
+	window.requestAnimationFrame(frameLooper);
+	fbc_array = new Uint8Array(analyser.frequencyBinCount);
+	analyser.getByteFrequencyData(fbc_array);
+	ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+	ctx.fillStyle = '#3B3644'; // Color of the bars
+	
+	bars = 200;
+	for (var i = 0; i < bars; i++) {
+		bar_x = i * 5;
+		bar_width = 4;
+		bar_height = -(fbc_array[i] / 2);
+		//  fillRect( x, y, width, height ) // Explanation of the parameters below
+		ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+	}
+}
